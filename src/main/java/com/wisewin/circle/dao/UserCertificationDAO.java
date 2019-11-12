e4ef3a453f8c3f009c47f5bb97982b9d15bc1048
wangbin <1533164866@qq.com>
2019-11-12 16:11:13 +0800

    xxx

---
+++ src/main/java/com/wisewin/circle/dao/UserCertificationDAO.java
@@ -0,0 +1,12 @@
+package com.wisewin.circle.dao;
+
+import com.wisewin.circle.entity.bo.UserCertification;
+
+/**
+ * @Author: Wang bin
+ * @date: Created in 13:58 2019/11/12
+ */
+public interface UserCertificationDAO {
+
+    int insertUserCertification(UserCertification userCertification);
+}
